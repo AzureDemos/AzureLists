0ca8d6c9d5651c034a49191225f5056a9e5458e6
Steve Leonard <steve-leonard@outlook.com>
2018-05-29 15:41:58 +0100

    enable cors on API

--- 101-AppServicesWithSQLDB/API/AzureLists.Api/App_Start/WebApiConfig.cs
+++ 101-AppServicesWithSQLDB/API/AzureLists.Api/App_Start/WebApiConfig.cs
@@ -1,6 +1,6 @@
 ﻿using AzureLists.Api.App_Start;
 using System.Web.Http;
-
+using System.Web.Http.Cors;
 namespace AzureLists.Api
 {
     public static class WebApiConfig
@@ -10,6 +10,9 @@ namespace AzureLists.Api
             config.MapHttpAttributeRoutes();
             config.RegisterDependencies();
             config.RegisterSwagger();
+
+            var cors = new EnableCorsAttribute("*","*","*");
+            config.EnableCors(cors);
         }
     }
 }
